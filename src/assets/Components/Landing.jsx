310571da06531bce8fdf71debb4ff453a198f1f6
SrujalYandagoudar <131784602+SrujalYandagoudar@users.noreply.github.com>
2025-01-23 13:46:29 +0530

    push

--- src/assets/Components/Landing.jsx
+++ src/assets/Components/Landing.jsx
@@ -9,10 +9,10 @@ export default function Landing() {
 
             <nav className="grid 2xl:grid-cols-8 md:grid-cols-6 lg:grid-cols-6  mx-4 max-[500px]:text-sm">
                 <ul className="flex justify-evenly items-center col-span-3 md:col-span-4 2xl:col-end-9 md:col-end-7 lg:col-end-7 md:gap-10 gap-6 pt-4 ">
-                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[14px]"><Link to="/" className="">Home</Link></li>
-                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[14px]"><Link to="/Project" className="">Recent Projects</Link></li>
-                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[14px]"><Link to="/About-Us" className="">About Us</Link></li>
-                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[14px]"><Link to="/Contact" className="">Contact</Link></li>
+                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[12px]"><Link to="/" className="">Home</Link></li>
+                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[12px]"><Link to="/Project" className="">Recent Projects</Link></li>
+                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[12px]"><Link to="/About-Us" className="">About Us</Link></li>
+                    <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[12px]"><Link to="/Contact" className="">Contact</Link></li>
                 </ul>
             </nav>
 
--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
 
          <nav className="grid 2xl:grid-cols-8 md:grid-cols-6 max-[1060px]:grid-cols-4 md:mx-4">
           <img src={logo} alt="ANANT ARCHITECTS logo" className="w-24 max-[700px]:col-span-4 max-[700px]:justify-self-center" />
-                  <ul className="flex justify-evenly items-center  col-span-4 2xl:col-end-9 md:col-end-7  md:gap-10 pt-4">
+                  <ul className="flex justify-evenly items-center  col-span-4 2xl:col-end-9 md:col-end-7  md:gap-10 max-[500px]:gap-4 pt-4">
                       <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[12px]"><Link to="/" className="">Home</Link></li>
                       <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[12px]"><Link to="/Project" className="">Recent Projects</Link></li>
                       <li className="text-[#00008B] hover:text-xl transform duration-300 font-semibold text-lg max-[500px]:text-[12px]"><Link to="/About-Us" className="">About Us</Link></li>
